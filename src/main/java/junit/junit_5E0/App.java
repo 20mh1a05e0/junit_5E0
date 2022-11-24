52d94197c309e9faaaa48e01d3eb47075414ccf7
Lenovo <Lenovo@DivyaSri>
2022-11-24 18:59:35 +0530

    my first junit test case

---
+++ src/main/java/junit/junit_5E0/App.java
@@ -0,0 +1,12 @@
+package junit.junit_5E0;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to devops lab" );
+    }
+    public int add(int a,int b)
+    {
+         return(a+b);    	
+    }
+}
